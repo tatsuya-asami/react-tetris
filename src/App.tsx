4b55371daa46bd1ae18c457a67605eb455e6daf3
astatsuya <sharekobekko@gmail.com>
2019-11-04 17:39:30 +0900

    set envrionment variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  return <div className="App">App.tsx</div>;
+  return (
+    <div className="App">
+      <p>App.tsx</p>
+      <p>environment variables:{process.env.REACT_APP_TEST}</p>
+      <p>nodenv {process.env.NODE_ENV}</p>
+    </div>
+  );
 };
 
 export default App;
