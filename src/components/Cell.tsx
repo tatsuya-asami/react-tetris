50060779635a4aa0a6c10cf04c8db1d180a7ec86
astatsuya <sharekobekko@gmail.com>
2019-11-24 11:39:34 +0900

    remove extra code

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
@@ -6,9 +6,8 @@ export interface ICell {
   type: 0 | 'I' | 'J' | 'L' | 'O' | 'S' | 'T' | 'Z';
 }
 
-const Cell: React.FC<ICell> = ({ type }) => {
-  // console.log('rerender');
-  return <StyledCell type={type} color={TETROMINOS[type].color} />;
-};
+const Cell: React.FC<ICell> = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color} />
+);
 // avoid rendering too many times
 export default React.memo(Cell);
